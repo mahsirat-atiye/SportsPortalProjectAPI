88bc00d22c8334e3aebe8c0c4a3d0dbd6195d7ce
mahsirat-atiye <atieh.atieh.jamshidpoor@gmail.com>
2019-01-25 00:06:05 +0330

    بازیکن

--- sport/views/football_views.py
+++ sport/views/football_views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
@@ -131,9 +133,95 @@ def football_team_detail_view(request, team_id):
 
 def football_player_detail_view(request, player_id):
     player = get_object_or_404(FootballPlayer, pk=player_id)
+    if request.POST and request.POST['part'] == 'for_season':
+        # todo
+        # مفهوم فصل در فوتبال با علی چک شود
+        season = request.POST['season']
+        season = int(season)
+        x = datetime.datetime(season, 1, 1)
+        y = datetime.datetime(season + 1, 1, 1)
+        events = player.footballevent_set.all().filter(exact_time__lte=y).filter(exact_time__gte=x)
+    else:
+        events = player.footballevent_set.all()
+
+    total_G = 0
+    total_PG = 0
+    total_YC = 0
+    total_RC = 0
+    total_E = 0
+    total_P = 0
+    total_CH = 0
+    total_CO = 0
+    total_SG = 0
+
+    for e in events:
+        if e.event_type == 'G':
+            total_G += 1
+        elif e.event_type == 'PG':
+            total_PG += 1
+        elif e.event_type == 'YC':
+            total_YC += 1
+        elif e.event_type == 'RC':
+            total_RC += 1
+        elif e.event_type == 'E':
+            total_E += 1
+        elif e.event_type == 'P':
+            total_P += 1
+        elif e.event_type == 'CH':
+            total_CH += 1
+        elif e.event_type == 'CO':
+            total_CO += 1
+        elif e.event_type == 'SG':
+            total_SG += 1
+
+    news = News.objects.all().order_by('-publish_date')
+    related_news = set([])
+    if request.POST and request.POST['part'] == 'filter_news':
+        if request.POST['choice'] == 'tag':
+            for n in news:
+                for t in n.tag_set.all():
+                    if t.text.__contains__(player.first_name) or t.text.__contains__(player.last_name):
+                        related_news.add(n)
+
+        elif request.POST['choice'] == 'title':
+            for n in news:
+
+                if n.title.__contains__(player.first_name) or n.title.__contains__(player.last_name):
+                    related_news.add(n)
+
+        else:
+            for n in news:
+
+                if n.text.__contains__(player.first_name) or n.text.__contains__(player.last_name):
+                    related_news.add(n)
+            pass
+    else:
+
+        for n in news:
+            for t in n.tag_set.all():
+                if t.text.__contains__(player.first_name) or t.text.__contains__(player.last_name):
+                    related_news.add(n)
+            if n.title.__contains__(player.first_name) or n.title.__contains__(player.last_name):
+                related_news.add(n)
+            if n.text.__contains__(player.first_name) or n.text.__contains__(player.last_name):
+                related_news.add(n)
+
+    related_news = list(related_news)
     context = {
-        'player': player
+        'player': player,
+        'related_news': related_news,
+        'total_G': total_G,
+        'total_PG': total_PG,
+        'total_YC': total_YC,
+        'total_RC': total_RC,
+        'total_E': total_E,
+        'total_P': total_P,
+        'total_CH': total_CH,
+        'total_CO': total_CO,
+        'total_SG': total_SG
+
     }
+
     return render(request, 'sport/football_player_detail.html', context)
 
 
