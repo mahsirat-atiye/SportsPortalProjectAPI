ed0e162ee4099bfe6f835f00ce52fa8fcb1f2897
mahsirat-atiye <atieh.atieh.jamshidpoor@gmail.com>
2019-01-26 23:17:13 +0330

    صفحه جزییات خبر

--- sport/views/general_views.py
+++ sport/views/general_views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
 
-from sport.models import News
+from sport.models import News, Comment
 
 
 class NewsDetailView(generic.DetailView):
@@ -12,6 +12,14 @@ class NewsDetailView(generic.DetailView):
 
 def news_detail_view(request, news_id):
     news = get_object_or_404(News, pk=news_id)
+
+    if request.POST:
+        title = request.POST['title']
+        text = request.POST['comment']
+        writer = request.user
+        new_comment = Comment(title=title, text=text, writer=writer, news=news)
+        new_comment.save()
+
     related_news = []
     related_tags = news.tag_set.all()
     for t in related_tags:
@@ -19,11 +27,12 @@ def news_detail_view(request, news_id):
             related_news.append(n)
     if related_news.__contains__(news):
         related_news.remove(news)
+    comments = news.comment_set.all()
 
     return render(request, 'sport/news_detail.html', {
         'news': news,
         'related_news': related_news,
-        'n': related_news.__len__(),
-        'll': ",,lkl",
+        "comments": comments
+
     }
                   )
